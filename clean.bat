194bd1c9ad803ba15e3d96c6ec5ef78b16d37285
Jessie Lesbian <jessielesbian@protonmail.com>
2022-11-11 15:18:44 +0100

    Revert "IKVMC now uses SharpZipLib from nuget"
    This reverts commit 4915bf0213a935a9fdcb1ea2e832c26cbff03e5c.

--- clean.bat
+++ clean.bat
@@ -11,8 +11,6 @@ rmdir /s /q runtime\obj
 rmdir /s /q ikvm\obj
 rmdir /s /q obj
 rmdir /s /q ikvmstub\obj
-rmdir /s /q nuget
-rmdir /s /q packages
 
 echo cleaning other files...
 FOR /F "usebackq delims=" %%A IN (".gitignore") DO DEL /F /Q /S "%%A" >nul 2>nul
